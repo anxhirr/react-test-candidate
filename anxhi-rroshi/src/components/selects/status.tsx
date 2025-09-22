212302d505e9c4ebc27fff19f35a9e6a0eb4424a
Anxhi <anxhirr@gmail.com>
2025-09-22 11:51:32 -0400

    fix: match status buttons with figma design

--- anxhi-rroshi/src/components/selects/status.tsx
+++ anxhi-rroshi/src/components/selects/status.tsx
@@ -1,35 +1,52 @@
 'use client';
 
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { statusToColor } from '@/lib/colors';
-import { Button } from '../ui/button';
 import { statusToLabel } from '@/lib/labels';
+import { cn } from '@/lib/utils';
 
 const STATUS_LIST: TaskT['status'][] = ['NEW', 'IN_PROGRESS', 'ON_HOLD', 'CACELLED', 'COMPLETED'];
 
 const StatusSelect = ({ onValueChange, value }: { onValueChange: (value: string) => void; value: string }) => {
 	return (
-		<div className="flex">
+		<div className="flex justify-center">
 			{STATUS_LIST.map((status) => {
 				const isActive = value === status;
+				const isFirst = STATUS_LIST.indexOf(status) === 0;
+				const isLast = STATUS_LIST.indexOf(status) === STATUS_LIST.length - 1;
 
 				const color = statusToColor(status);
 				return (
-					<Button
-						type="button"
-						key={status}
-						onClick={() => {
-							onValueChange(status);
-						}}
-						variant={isActive ? 'default' : 'outline'}
-						style={{
-							color: isActive ? undefined : color,
-							borderColor: color,
-							backgroundColor: isActive ? color : undefined,
-						}}
-					>
-						<span>{statusToLabel(status)}</span>
-					</Button>
+					<div className="relative">
+						<button
+							type="button"
+							key={status}
+							onClick={() => {
+								onValueChange(status);
+							}}
+							style={{
+								borderColor: color,
+								backgroundColor: color,
+								color: 'white',
+							}}
+							className={cn(
+								'p-5 status-button min-w-[12rem] h-[66px] gap-2 select-none cursor-pointer',
+								isFirst && 'rounded-l-full',
+								isLast && 'rounded-r-full'
+							)}
+						>
+							<span>{statusToLabel(status)}</span>
+						</button>
+						<div
+							className={cn(
+								'w-0 h-0 border-t-[33px] border-t-transparent border-l-[50px] border-b-[33px] border-b-transparent',
+								'absolute top-0 -right-[50px] z-10',
+								isLast && 'hidden'
+							)}
+							style={{
+								borderLeftColor: color,
+							}}
+						/>
+					</div>
 				);
 			})}
 		</div>
