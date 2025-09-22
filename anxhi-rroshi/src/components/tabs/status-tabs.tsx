ff77714597b25bf3b2b9d722d03c54749390ea1c
Anxhi <anxhirr@gmail.com>
2025-09-22 04:28:50 -0400

    feat: -install all shadcn ui components -status tabs initial look

---
+++ anxhi-rroshi/src/components/tabs/status-tabs.tsx
@@ -0,0 +1,19 @@
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
+const STATUS_LIST = ['NEW', 'IN_PROGRESS', 'ON_HOLD', 'CACELLED', 'COMPLETED'];
+
+const StatusTabs = () => {
+	return (
+		<div className="flex w-full max-w-sm flex-col gap-6">
+			<Tabs defaultValue="account">
+				<TabsList>
+					{STATUS_LIST.map((status) => {
+						return <TabsTrigger value={status}>{status}</TabsTrigger>;
+					})}
+				</TabsList>
+			</Tabs>
+		</div>
+	);
+};
+
+export { StatusTabs };
